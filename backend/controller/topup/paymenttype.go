fb7e59253eb2aefd245f8499a58e68ba6555d180
chonticha1844 <111478571+chonticha1844@users.noreply.github.com>
2023-01-31 17:00:23 +0700

    update backend top up - close #122

--- backend/controller/topup/paymenttype.go
+++ backend/controller/topup/paymenttype.go
@@ -9,46 +9,46 @@ import (
 
 // POST--paymenttype--
 func CreatePaymentType(c *gin.Context) {
-	var paymenttype entity.PaymentType
-	if err := c.ShouldBindJSON(&paymenttype); err != nil {
+	var payment_type entity.PaymentType
+	if err := c.ShouldBindJSON(&payment_type); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if err := entity.DB().Create(&paymenttype).Error; err != nil {
+	if err := entity.DB().Create(&payment_type).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": paymenttype})
+	c.JSON(http.StatusOK, gin.H{"data": payment_type})
 }
 
 //GET--paymenttype id--
 
 func GetPaymentType(c *gin.Context) {
-	var paymenttype entity.PaymentType
+	var payment_type entity.PaymentType
 	id := c.Param("id")
-	if err := entity.DB().Raw("SELECT * FROM paymenttypes WHERE id = ?", id).Scan(&paymenttype).Error; err != nil {
+	if err := entity.DB().Raw("SELECT * FROM payment_types WHERE id = ?", id).Scan(&payment_type).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": paymenttype})
+	c.JSON(http.StatusOK, gin.H{"data": payment_type})
 }
 
 // GET--paymenttypes--
 func ListPaymentTypes(c *gin.Context) {
-	var paymenttypes []entity.PaymentType
-	if err := entity.DB().Raw("SELECT * FROM paymenttypes").Scan(&paymenttypes).Error; err != nil {
+	var payment_types []entity.PaymentType
+	if err := entity.DB().Raw("SELECT * FROM payment_types").Scan(&payment_types).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": paymenttypes})
+	c.JSON(http.StatusOK, gin.H{"data": payment_types})
 }
 
 // DELETE--paymenttype id--
 func DeletePaymentType(c *gin.Context) {
 	id := c.Param("id")
-	if tx := entity.DB().Exec("DELETE FROM paymenttypes WHERE id = ?", id); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "paymenttype not found"})
+	if tx := entity.DB().Exec("DELETE FROM payment_types WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "payment_type not found"})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": id})
@@ -56,22 +56,22 @@ func DeletePaymentType(c *gin.Context) {
 
 // PATCH--paymenttype--
 func UpdatePaymentType(c *gin.Context) {
-	var paymenttype entity.PaymentType
-	if err := c.ShouldBindJSON(&paymenttype); err != nil {
+	var payment_type entity.PaymentType
+	if err := c.ShouldBindJSON(&payment_type); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if tx := entity.DB().Where("id = ?", paymenttype.ID).First(&paymenttype); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "paymenttype not found"})
+	if tx := entity.DB().Where("id = ?", payment_type.ID).First(&payment_type); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "payment_type not found"})
 		return
 	}
 
-	if err := entity.DB().Save(&paymenttype).Error; err != nil {
+	if err := entity.DB().Save(&payment_type).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": paymenttype})
+	c.JSON(http.StatusOK, gin.H{"data": payment_type})
 }
 
 //75
