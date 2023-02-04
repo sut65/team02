b416dbffcad1cbaa6f7b85d2c3079616f895c9cb
WanwisaTT <wanisa7758@gmail.com>
2023-02-04 14:42:34 +0700

    update bookshelf interface - close #152

--- frontend/app3/src/interfaces/IReader.ts
+++ frontend/app3/src/interfaces/IReader.ts
@@ -2,14 +2,14 @@ import { GenderInterface } from "./IGender";
 import { PrefixInterface } from "./IPrefix";
 
 export interface ReaderInterface {
-    ID?:number;
-    Email?: string;
-    PrefixID?:   number;
-	Prefix?: PrefixInterface;
-    Name?: string;
-    Nickname?: string;
-    GenderID?: number;
-    Gender?: GenderInterface;
-    Date_of_Birth:Date | null;
-    Password?: string;
+    ID?:number,
+    Email?: string,
+    PrefixID?:   number,
+	Prefix?: PrefixInterface,
+    Name?: string,
+    Nickname?: string,
+    GenderID?: number,
+    Gender?: GenderInterface,
+    Date_of_Birth:Date | null,
+    Password?: string,
 }
---
+++ frontend/app3/src/interfaces/bookshelf/IBookshelf_Number.ts
@@ -0,0 +1,8 @@
+import { ReaderInterface } from "../IReader";
+
+export interface Bookshelf_NumberInterface {
+    ID?: number,
+    ReaderID: number,
+    Reader: ReaderInterface,
+    Bookshelf_Name: string,
+}
