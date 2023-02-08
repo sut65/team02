788675054ad425dd85d2b11f2feec399bd1b84be
WanwisaTT <wanisa7758@gmail.com>
2023-02-09 00:07:34 +0700

    ui module equipment - close #173

--- frontend/app3/src/App.tsx
+++ frontend/app3/src/App.tsx
@@ -64,36 +64,9 @@ export default function App() {
             <Route path="/report-fictions" element={<ReportFictionData/>}/>
             <Route path="/report-fiction/create/:id" element={<ReportFictionCreate/>}/>
             <Route path="/report-fiction/update/:id" element={<ReportFictionUpdate/>}/>
+            <Route path="/top_up/create/" element={<TopUpCreate/>}/>
         </Routes>
         </div>
     </Router>
     );
-
-  
-
-  return (
-  <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} /> 
-        <Route path="/fictions" element={<FictionInfo  />} /> 
-        <Route path="/fiction/:id" element={<FictionInfoDetail />} />
-        {/* <Route path="/fictions-show" element={<ShowFictions  />} />  */}
-        <Route path="/fiction/story/:id" element={<ShowStory />} />
-        <Route path="/feedback-create/" element={<FeedbackCreate />}/>
-        <Route path="/feedback-update/:id" element={<FeedbackUpdate/>}/>
-        <Route path="/feedbacks" element={<FeedbackTable/>}/>
-        <Route path="/reader-profile" element={<ReaderProfile />}/>
-        <Route path="/reviews" element={<ReviewTable/>}/>
-        <Route path="/review/create/:id" element={<ReviewCreate/>}/>
-        <Route path="/review/update/:id" element={<ReviewUpdate/>}/>
-        <Route path="/top_ups" element={<TopUpTable/>}/>
-        <Route path="/reader-create" element={<ReaderCreate/>}/>
-        <Route path="/bookshelf_create" element={<Bookshelf/>}/>
-        <Route path="/top_up/create/" element={<TopUpCreate/>}/>
-      </Routes>
-    </div>
-  </Router>
-  );
 }
