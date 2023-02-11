31d3d4eba15b1ebd546b8de1799efc67612c394f
Nanthat <phaitd.n29@gmail.com>
2023-02-11 21:51:35 +0700

    updated - close #191

--- frontend/app3/src/components/feedback/FeedbackTable.tsx
+++ frontend/app3/src/components/feedback/FeedbackTable.tsx
@@ -77,6 +77,7 @@ function FeedbackTable () {
         }
         getFeedbackByFID();
         setOpenDeleteFeedback(false)
+        window.location.href = "/feedbacks";
     }
 
     useEffect(() => {
